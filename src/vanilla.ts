dc9ebac0287010e3a90123a309cdd7a797fde370
Mateus Souza <mateussouzaweb@gmail.com>
2021-03-01 12:49:32 +0000

    Version 1.0.8

--- src/vanilla.ts
+++ src/vanilla.ts
@@ -1,5 +1,5 @@
 /*! Vanilla UI - https://github.com/mateussouzaweb/vanilla-ui */
-export const __version = '1.0.7'
+export const __version = '1.0.8'
 
 export * from "./core/utils"
 export * from "./core/selector"

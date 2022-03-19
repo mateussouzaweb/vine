32aab2060fd85d067573e5adc6f5fdf75294165b
Mateus Souza <mateussouzaweb@gmail.com>
2022-03-19 16:06:08 -0300

    Store helper has been remove to promote core methods since there is no significant change

--- src/vanilla.ts
+++ src/vanilla.ts
@@ -7,7 +7,6 @@ export * from "./core/events"
 export * from "./core/observers"
 export * from "./core/component"
 
-export * as store from "./extra/store"
 export * as http from "./extra/http"
 export * as route from "./extra/route"
 export * as template from "./extra/template"
